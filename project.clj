8fe6e25a6826fd04a2c7032a987c34def498ac20
Jayaraj Poroor <jayaraj.poroor@gmail.com>
2014-02-19 18:20:14 +0530

    initial commit

---
+++ project.clj
@@ -0,0 +1,23 @@
+; Copyright (c) Jayaraj Poroor. All rights reserved.
+; The use and distribution terms for this software are covered by the
+; GNU Lesser General Public License 3.0 
+; (http://www.gnu.org/copyleft/lesser.html)
+; which can be found in the file agpl-3.0.html at the root of this distribution.
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+; You must not remove this notice, or any other, from this software.
+
+(defproject generuse-jdbc "0.1.0-SNAPSHOT"
+  :description "Generuse JDBC module"
+  :url "http://generuse.org"
+  :license {:name "GNU Lesser General Public License 3.0"
+            :url "http://www.gnu.org/copyleft/lesser.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.generuse/generuse-lib "0.1.0-SNAPSHOT"]  
+				]
+  :main generuse.mod.jdbc
+  :aot  [generuse.mod.jdbc]
+  :repositories {"sonatype-oss-public"
+               "https://oss.sonatype.org/content/groups/public/"}  
+
+)
